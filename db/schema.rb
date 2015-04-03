636938ad83e09b3701c3e0e2b9c24ad4b6f2cb00
dhiraj <dheeraj.dc.91@gmail.com>
2015-04-03 12:59:53 +0530

    User controller created and model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402100755) do
+ActiveRecord::Schema.define(version: 20150403072318) do
 
   create_table "adminsections", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,5 +30,16 @@ ActiveRecord::Schema.define(version: 20150402100755) do
 
   add_index "adminsections", ["email"], name: "index_adminsections_on_email", unique: true
   add_index "adminsections", ["reset_password_token"], name: "index_adminsections_on_reset_password_token", unique: true
+
+  create_table "users", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.date     "dob"
+    t.decimal  "contact"
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
