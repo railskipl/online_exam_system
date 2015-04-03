2429916fc3d00d23c5d3bcc5ea05374547e785e9
railskipl <rails@kunalinfotech.net>
2015-04-03 16:24:19 +0530

    Delete test.rb

--- app/models/test.rb
+++
@@ -1,5 +0,0 @@
-class Test < ActiveRecord::Base
-	has_many :questions, dependent: :destroy
-	accepts_nested_attributes_for :questions, :allow_destroy => true
-	validates :name, :presence => true
-end
