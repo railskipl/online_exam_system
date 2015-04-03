ec1536035c96b3c6d76f470d3ece4a2a7097db5a
Sushil Deothakle <sushildeothakle@gmail.com>
2015-04-03 12:04:01 +0530

    my first commit

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,14 @@
+class AdminsController < ApplicationController
+before_filter :authenticate_adminsection!
+
+
+def index
+		
+	end	
+def add_new_test
+	
+end
+def result
+	
+end
+end
---
+++ test/models/adminsection_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AdminsectionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
