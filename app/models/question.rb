01dcf8c2c7eea17c60fd17556964492dc872b519
atish <aatish93maske@gmail.com>
2015-04-03 15:40:22 +0530

    added twitter-bootstrap and answer module

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ActiveRecord::Base
+	belongs_to :test
+		has_many :answers, dependent: :destroy
+		accepts_nested_attributes_for :answers, :allow_destroy => true
+		validates :content, :presence => true
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ActiveRecord::Base
+	has_many :questions, dependent: :destroy
+	accepts_nested_attributes_for :questions, :allow_destroy => true
+	validates :name, :presence => true
 end
