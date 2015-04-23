4f77b21bea61810b0335abb6b5af1078504df44f
dhiraj <dheeraj.dc.91@gmail.com>
2015-04-23 14:23:46 +0530

    added in application.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,23 +2,23 @@ class User < ActiveRecord::Base
 
    # validates_format_of :firstname, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/  
 
-    validates_format_of :lastname, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
+    #validates_format_of :lastname, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
     
  belongs_to :exam
 
 #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-#  validates :firstname, :lastname,
-#              presence: true,
+  validates :firstname, :lastname,
+              presence: true,
 
-# #              #validates_length_of :user_name, :in => 6..12 , :message => " must be 6 to 12 characters"
+              validates_length_of :user_name, :in => 3..50 , :message => " must be 6 to 12 characters"
 # #              length: { maximum: 12 }
 # #             #:length => { :maximum => 12 }
 # #             #uniqueness: true,
-# #             format: {
-# #               with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ ,
-# #               message: "must be formatted correctly"
-# #             }
+             format: {
+               with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ ,
+               message: "must be formatted correctly"
+             }
 
 # # # validates     {:case_sensitive => false, message:  "Email already exists!"}        \A([^@\s]+)@((?:[-a-z]+\.)+[a-z]{2,})\z
 
