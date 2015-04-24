029e58ea3cd9d25c324a5a719727394c39a2b6b1
dhiraj <dheeraj.dc.91@gmail.com>
2015-04-24 18:01:05 +0530

    design changes method added so question field and answer filed must be present

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,17 +1,7 @@
 class Answer < ActiveRecord::Base
-
-	
-	belongs_to :questions
-
 	belongs_to :question
-
-	validates :content, :presence => true
 	belongs_to :result
-
-def duplicate_answer?
-  if answers.all?{|attr| self[attr].present?}
-    errors.add :base, "Answer should be present "
-  end
-end
+ 	validates :content, :presence => true
+ 	#validates :correct_answer, acceptance: true
 end
 
