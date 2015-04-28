7e4b239aee0e1bda68ea6d49f6451b602bc0c5a4
Sushil Deothakle <sushildeothakle@gmail.com>
2015-04-28 15:49:37 +0530

    change in css

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module OnlineExamSystem
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+Rails.application.config.assets.precompile += %w( bootstrap_confirm.js jquery-ui.css)
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_record.default_timezone = :local
